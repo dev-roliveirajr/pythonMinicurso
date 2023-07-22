db6b81a68f5bf129ef6388679867175947a7e14b
dev-roliveirajr <dev.roliveirajr@gmail.com>
2023-07-22 18:09:48 -0300

    Meu primeiro commit

---
+++ Relatorio.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import utils_email
+
+# importar base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+
+# visualizar a base de dados
+pd.set_option('display.max_columns', None)
+#print(tabela_vendas)
+
+
+# faturamento por loja
+#faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+# print(faturamento)
+
+# qtd de produtos vendidos por loja
+#quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+#print(quantidade)
+
+# ticket médio por produto, por loja
+#ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+#print(ticket_medio)
+
+# ticket médio por produto, por loja com subtotal
+#ticket_medio_produto = tabela_vendas[['Produto', 'Valor Final']].groupby(['Produto']).mean()
+
+#ticket_medio_produto_loja = tabela_vendas[['ID Loja', 'Produto', 'Valor Final']].groupby(['Produto', 'ID Loja']).mean()
+#print(ticket_medio_produto_loja)
+
+tabela_vendas['Ticket Medio Produto'] = tabela_vendas.groupby('Produto')['Valor Final'].transform('mean')
+tabela_vendas['Ticket Medio Produto Loja'] = tabela_vendas.groupby(['Produto', 'ID Loja'])['Valor Final'].transform('mean')
+#print(tabela_vendas)
+
+report = tabela_vendas[['Produto', 'Ticket Medio Produto', 'ID Loja', 'Ticket Medio Produto Loja']].loc[tabela_vendas['Produto'] == 'Bermuda'].groupby(['Produto', 'Ticket Medio Produto', 'ID Loja']).mean()
+
+#print(report)
+
+
+# enviar email com o relatório
+email = utils_email.Email()
+mensagem = f"<P>Olá,</P><P>Segue o relatório semanal de vendas:</P>{report.to_html()}<P>Obrigado.</P>"
+email.enviar_email("", "cihadak356@muzitp.com", "Relatório Semanal de Vendas", mensagem)
---
+++ utils_email.py
@@ -0,0 +1,60 @@
+import smtplib
+import email.message
+
+class Email:
+
+    def __init__(self):
+        self.remetente = ""
+        self.senha = ""
+        parametros = self.config_email()
+        for p in parametros:
+            if p[0] == "remetente": self.remetente = p[1]
+            if p[0] == "senha": self.senha = p[1]
+
+    def enviar_email(self, remetente, destinatario, assunto, mensagem):
+        global _remetente
+        global _senha
+
+        if remetente == "":
+            remetente = self.remetente
+
+        msg = email.message.Message()
+        msg["Subject"] = assunto
+        msg["From"] = remetente
+        msg["To"] = destinatario
+        password = self.senha
+        msg.add_header("Content-Type", "text/html")
+        msg.set_payload(mensagem)
+
+        s = smtplib.SMTP("smtp.gmail.com: 587")
+        s.starttls()
+        s.login(msg["From"], password)
+        s.sendmail(msg["From"], [msg["To"]], msg.as_string().encode("utf-8"))
+
+        return True
+
+    def config_email(self):
+        parametros = []
+        with open("email_config.txt") as arquivo:
+            linhas = arquivo.readlines()
+            arquivo.close()
+
+        for linha in linhas:
+            if "remetente" in linha:
+                parametros.append(["remetente", linha.split("=")[1].strip().replace('"', '').replace('\n', '')])
+
+            if "senha" in linha:
+                parametros.append(["senha", linha.split("=")[1].strip().replace('"', '').replace('\n', '')])
+
+        return parametros
+
+
+def test_parametros():
+    email = Email()
+    assert [email.remetente, email.senha] != ["", ""]
+
+
+def test_email():
+    email = Email()
+    mensagem = "<P>Olá,</P><P>Segue o relatório semanal de vendas:</P><PRE>==== Corpo da mensagem ====</PRE><P>Obrigado.</P>"
+    assert email.enviar_email(email.remetente, "cihadak356@muzitp.com", "Relatório Semanal", mensagem) == True
