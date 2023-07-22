a40259318cac6e14ee594029eac79cec79ab2e02
dev-roliveirajr <dev.roliveirajr@gmail.com>
2023-07-22 18:18:38 -0300

    Separar os testes basicos do Pandas do programa de envio do relatório.

--- Relatorio.py
+++ Relatorio.py
@@ -9,24 +9,6 @@ pd.set_option('display.max_columns', None)
 #print(tabela_vendas)
 
 
-# faturamento por loja
-#faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
-# print(faturamento)
-
-# qtd de produtos vendidos por loja
-#quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
-#print(quantidade)
-
-# ticket médio por produto, por loja
-#ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
-#print(ticket_medio)
-
-# ticket médio por produto, por loja com subtotal
-#ticket_medio_produto = tabela_vendas[['Produto', 'Valor Final']].groupby(['Produto']).mean()
-
-#ticket_medio_produto_loja = tabela_vendas[['ID Loja', 'Produto', 'Valor Final']].groupby(['Produto', 'ID Loja']).mean()
-#print(ticket_medio_produto_loja)
-
 tabela_vendas['Ticket Medio Produto'] = tabela_vendas.groupby('Produto')['Valor Final'].transform('mean')
 tabela_vendas['Ticket Medio Produto Loja'] = tabela_vendas.groupby(['Produto', 'ID Loja'])['Valor Final'].transform('mean')
 #print(tabela_vendas)
